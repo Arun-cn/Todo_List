1fff2d791e0b50474c9918254c4996c13fc6988d
Arun Chandran <53257113+arunchandran409@users.noreply.github.com>
2023-03-16 13:39:15 +0530

    add when checkbox change update text style

--- src/components/TodoItemCard.jsx
+++ src/components/TodoItemCard.jsx
@@ -1,23 +1,25 @@
 import '../styles/todoitemcard.css';
+import { useState } from 'react';
 
 function TodoItemCard (props){
-
+    const [isChecked, setIsChecked] = useState(props.completed);
     const handleDelete= (e)=>{
         const id = props.itemId;
        props.onButtonClick(id);
     }
 
     const handleCompleted = (e)=>{
-        e.preventDefault();
+  
         props.onCheck(props.itemId)
+        setIsChecked(!isChecked);
     }
 
 
     return(
         <div className='cardcontiner'>
             <div className='cardleftside'>
-                <input type="checkbox" checked={props.completed} name="isDone" id="checker" onClick={handleCompleted}/>
-                <label style={{ color: props.completed ? 'green' : 'red' }}>{props.item} </label>
+                <input type="checkbox" checked={isChecked} name="isDone" id="checker" onChange={handleCompleted}/>
+                <label style={{ textDecoration: isChecked ? 'line-through' : 'none' }}>{props.item} </label>
             </div>
             
             <div className='cardrightside'>
